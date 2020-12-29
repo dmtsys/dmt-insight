64ab8af62feae0ff2d1bf03ca279cbabe0d9e958
bluwy <bjornlu.dev@gmail.com>
2020-12-30 00:21:39 +0800

    Refactor nav link

--- src/components/NavLink.svelte
+++ src/components/NavLink.svelte
@@ -9,27 +9,17 @@
     position: relative;
     padding: 0.8rem 1rem;
     color: var(--dmt-cool-green);
-    text-decoration: none;
-  }
-
-  a::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    background-color: rgba(var(--dmt-cool-green-rgb), 0.15);
     border-radius: 0.5rem;
-    background-color: var(--dmt-cool-green);
-    opacity: 0.15;
-    transition: opacity 0.2s;
+    text-decoration: none;
+    transition: color 0.2s;
   }
 
-  a:hover::before {
-    opacity: 0.2;
+  a:hover {
+    background-color: rgba(var(--dmt-cool-green-rgb), 0.2);
   }
 
-  a:focus::before {
-    opacity: 0.25;
+  a:hover {
+    background-color: rgba(var(--dmt-cool-green-rgb), 0.25);
   }
 </style>
